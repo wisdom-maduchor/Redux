8a26f050229d4e7bea7208e68f023d2ba0b5ec53
wisdom-maduchor <maduchorwisdom@gmail.com>
2025-10-01 17:33:18 +0100

    react + redux increment and decrement

--- test-project/src/components/Counter.js
+++ test-project/src/components/Counter.js
@@ -1,12 +1,27 @@
+import {useSelector, useDispatch} from 'react-redux'
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+
+  const IncrementDispatchHandler = () => {
+    dispatch({type: 'increment'})
+  };
+  const DecrementDispatchHandler = () => {
+    dispatch({type: 'decrement'})
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={IncrementDispatchHandler}>Increment</button>
+        <button onClick={DecrementDispatchHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- test-project/src/store/index.js
+++ test-project/src/store/index.js
@@ -1,4 +1,4 @@
-import {redux, createStore} from 'react'
+import {createStore} from 'redux'
 
 const counterReducer = (state = {counter: 0}, action) => {
    if(action.type === 'increment') {
